62ef30b33e21a334bceaf2a3ab3c573c09e31132
Andrea Vidal <11.andrea.v.p@gmail.com>
2021-03-10 08:03:14 +0100

    layouts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container fluid={true}>
+        <Row as={"header"} className="cabecera">
+          <Col as={"h1"}>Título</Col>
+        </Row>
+        <Row as={"main"} >
+          <Col as={"section"} className="contenido" sm={12} md={9}>
+            <h2>Titular de la noticia</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo asperiores libero aliquam vero,maioresab.Voluptati busas sumenda porro nihil voluptatum quam ad tempora, accusantium quos quaerat fugiat incidunt sequi nemo.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo asperiores libero aliquam vero,maioresab.Voluptati busas sumenda porro nihil voluptatum quam ad tempora, accusantium quos quaerat fugiat incidunt sequi nemo.</p>
+          </Col>
+          <Col as={"section"} className="sidebar" sm={12} md={3}>
+            <h2>Categorías</h2>
+            <ul>
+              <li>Noticias de deporte</li>
+              <li>Noticias de ocio</li>
+              <li>Noticias de política</li>
+              <li>Noticias de espectáculos</li>
+            </ul>
+            <h2>Otras cosas</h2>
+            <ul>
+              <li>Nosequé</li>
+              <li>Nosecuánto</li>
+              <li>Talycual</li>
+            </ul>
+          </Col>
+        </Row>
+        <Row as={"footer"} className="pie">
+          <p >Texto del footer</p>
+        </Row>
+      </Container>
+    </>
   );
 }
 
